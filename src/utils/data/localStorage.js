443b967395d159070607e4f13e8381d843ba49fc
gyu jae <wjdrbwo1206@naver.com>
2022-05-05 23:08:09 +0900

    feat: localStorage 함수 추가

--- src/utils/data/localStorage.js
+++ src/utils/data/localStorage.js
@@ -1,4 +1,4 @@
-import moment from "moment"
+import moment from 'moment'
 
 const MAIN_STORAGE_KEY = 'todo'
 
@@ -8,28 +8,28 @@ export const getAllData = () => {
 }
 export const updateAllData = (allData) => {
   try {
-    localStorage.setItem(MAIN_STORAGE_KEY, JSON.stringify(allData))    
-  } catch(err) {
+    localStorage.setItem(MAIN_STORAGE_KEY, JSON.stringify(allData))
+  } catch (err) {
     console.log(err)
   }
 }
 
 export const getUserByNickName = (nickName) => {
   const allData = localStorage.getItem(MAIN_STORAGE_KEY)
-  return JSON.parse(allData).filter(val => val.userNickName === nickName)[0]
+  return JSON.parse(allData).filter((val) => val.userNickName === nickName)[0]
 }
 export const getUserByUserId = (userId) => {
   const allData = localStorage.getItem(MAIN_STORAGE_KEY)
-  return JSON.parse(allData).filter(val => val.id === userId)[0]
+  return JSON.parse(allData).filter((val) => val.id === userId)[0]
 }
-
 
 export const getCategoryByNickNameAndCategoryId = (nickName, categoryId) => {
   const user = getUserByNickName(nickName)
-  return user.data.category.filter(val => val.id === categoryId)[0]
+  return user.data.category.filter((val) => val.id === categoryId)[0]
 }
+
 export const getPastTodosByNickName = (nickName) => {
   const userData = getUserByNickName(nickName)
   const today = moment().format('YYYY/MM/DD')
-  return userData.data.todoList.filter(todo => moment(todo.date).isBefore(today) && !todo.isDone)
+  return userData.data.todoList.filter((todo) => moment(todo.date).isBefore(today) && !todo.isDone)
 }
