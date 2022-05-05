9ab9adb77055ccff7b3f3c8b2a97bbe61b2578e5
daseuls <dls2tmfs2@naver.com>
2022-05-05 21:10:21 +0900

    feat:userName state 생성, onChange event

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,10 +1,17 @@
+import React, { useState } from 'react'
 import styles from './Login.module.scss'
 
 function Login() {
+  const [userName, setUserName] = useState('')
+
+  const onChangeInputValue = (e) => {
+    setUserName(e.currentTarget.value)
+  }
+
   return (
     <div className={styles.loginWrapper}>
       <p className={styles.loginTitle}>닉네임을 입력해주세요</p>
-      <input className={styles.loginInput} />
+      <input onChange={onChangeInputValue} className={styles.loginInput} />
       <p className={styles.warningMessasge}>닉네임은 15자 이하로 설정해주세요</p>
       <div className={styles.loginBtn}>Login</div>
     </div>
