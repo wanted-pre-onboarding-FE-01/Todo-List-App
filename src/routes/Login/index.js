0e83429249ba32505899be54880358a4d48ae722
daseuls <dls2tmfs2@naver.com>
2022-05-05 20:46:41 +0900

    feat: login layout

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,7 +1,14 @@
 import styles from './Login.module.scss'
 
 function Login() {
-  return <div className={styles.loginWrapper}>로그인 페이지 테스트</div>
+  return (
+    <div className={styles.loginWrapper}>
+      <p className={styles.loginTitle}>닉네임을 입력해주세요</p>
+      <input className={styles.loginInput} />
+      <p className={styles.warningMessasge}>닉네임은 15자 이하로 설정해주세요</p>
+      <div className={styles.loginBtn}>Login</div>
+    </div>
+  )
 }
 
 export default Login
