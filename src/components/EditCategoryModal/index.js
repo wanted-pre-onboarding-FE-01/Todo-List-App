06428f0e203b94979be90d4393b925e0bc18f790
solchan98 <dev.sol0427@gmail.com>
2022-05-08 00:04:46 +0900

    feat: 컬러피커를 통한 동적으로 컬러 추가 기능 구현 및 useColorPicker 커스텀 훅 작성

--- src/components/EditCategoryModal/index.js
+++ src/components/EditCategoryModal/index.js
@@ -2,37 +2,31 @@ import styles from "./EditCategoryModal.module.scss"
 import PropTypes from 'prop-types'
 import { IoIosClose } from 'react-icons/io'
 import { useState } from "react"
-
-/** TODO
- * MORE_COLOR_LIST와 BRIEF_COLOR_LIST는 다른 파트분들과 공통으로 사용되는 데이터이다.
- * 따라서 데이터 관리를 논의해본 뒤, 리팩토링 예정이다.
- */
-const MORE_COLOR_LIST = [ 
-  { color: '#d4697b', isCheck: false }, { color: '#d27a56', isCheck: false }, { color: '#d1a345', isCheck: false }, { color: '#53a787', isCheck: false },
-  { color: '#566dda', isCheck: false }, { color: '#8450da', isCheck: false }, { color: '#787d84', isCheck: false }, { color: '#f7d7de', isCheck: false },
-  { color: '#f8dbcb', isCheck: false }, { color: '#f7eeb3', isCheck: false }, { color: '#d4f3d6', isCheck: false }, { color: '#d0e1fc', isCheck: false },
-  { color: '#e0d1f7', isCheck: false }, { color: '#d9dbdb', isCheck: false }]
+import { HexColorPicker } from "react-colorful"
+import { useColorPicker } from "../../hooks/ColorPicker"
 
 const BRIEF_COLOR_LIST = [ 
-  { color: '#d4697b', isCheck: false }, { color: '#d27a56', isCheck: false }, { color: '#d1a345', isCheck: false }, { color: '#53a787', isCheck: false },
-  { color: '#566dda', isCheck: false }]
-  /** */
+  { color: '#d4697b', isCheck: false }, { color: '#d27a56', isCheck: false }, { color: '#d1a345', isCheck: false }, { color: '#53a787', isCheck: false },{ color: '#566dda', isCheck: false }]
 
-function EditCategoryModal({ isShow, category, close, edit, remove }) {
+function EditCategoryModal({ isShow, category, close, edit, remove, setCategory }) {
 
-  const [moreOn, setMoreOn] = useState(false)
   const [colorList, setColorList] = useState(BRIEF_COLOR_LIST)
 
-  const [selectColor, setSelectColor] = useState(category.color)
+  const [moreOn, setMoreOn] = useState(false)
+
+  const moreButtonHandler = () => {
+    setMoreOn(prev => !prev)
+  }
+
+  const [newColor, changeNewColorHandler, addColorHandler] = useColorPicker(BRIEF_COLOR_LIST[0].color, setColorList, setMoreOn)
+
   const [categoryName, setCategoryName] = useState(category.categoryName)
+
   const categoryNameChangeHandler = (e) => {
     setCategoryName(e.currentTarget.value)
   }
 
-  const moreButtonHandler = () => {
-    setMoreOn(prev => !prev)
-    setColorList(prev => prev.length === MORE_COLOR_LIST.length ? BRIEF_COLOR_LIST : MORE_COLOR_LIST)
-  }
+  const [selectColor, setSelectColor] = useState(category.color)
 
   const colorSelectHandler = (e) => {
     const {dataset} = e.currentTarget
@@ -52,7 +46,7 @@ function EditCategoryModal({ isShow, category, close, edit, remove }) {
         </div>
         <ul className={styles.colorCircleList} >
           {colorList.map((color) => 
-            <li key={`color_index_${color.id}`} className={styles.colorCircle}>
+            <li key={`color_index_${color.color}`} className={styles.colorCircle}>
               <button
                 type='button'
                 aria-label="Select color"
@@ -62,18 +56,22 @@ function EditCategoryModal({ isShow, category, close, edit, remove }) {
                 onClick={colorSelectHandler}
               />
             </li>)}
-          {!moreOn && 
-            <li className={styles.colorCircle}>
-              <button type='button' className={styles.addColor} onClick={moreButtonHandler} aria-label="More button"/>
-            </li>}
+          <li className={styles.colorCircle}>
+            <button type='button' className={styles.addColor} onClick={moreButtonHandler} aria-label="More button"/>
+          </li>
         </ul>
         <div className={styles.inputWrapper}>
           <input type='text' value={categoryName} maxLength={20} placeholder={category.categoryName} onChange={categoryNameChangeHandler}/>
         </div>
         <div className={styles.btnsWrapper}>
-          <button type='button' className={styles.btn} onClick={() => edit(category.id, categoryName, selectColor )}>Edit</button>
-          <button type='button' className={styles.btn} onClick={() => remove(category.id)}>Delete</button>
+          <button type='button' className={styles.btn} onClick={() => edit(category.id, categoryName, selectColor, setCategory, setCategoryName )}>Edit</button>
+          <button type='button' className={styles.btn} onClick={() => remove(category.id, setCategory, setCategoryName)}>Delete</button>
         </div>
+        { moreOn && 
+        <div className={styles.colorPickerWrapper}>
+          <HexColorPicker style={{width: '100px', height: '100px', opacity: '80%'}} color={newColor} onChange={changeNewColorHandler} />
+          <button className={styles.colorPickerBtn} type="button" onClick={addColorHandler}>OK</button>
+        </div>}
       </div>
     </div>
   )
@@ -82,13 +80,14 @@ function EditCategoryModal({ isShow, category, close, edit, remove }) {
 EditCategoryModal.propTypes = {
   isShow: PropTypes.bool.isRequired,
   category: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     categoryName: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
   }),
   close: PropTypes.func.isRequired,
   edit: PropTypes.func.isRequired,
   remove: PropTypes.func.isRequired,
+  setCategory: PropTypes.func.isRequired,
 }
 
 export default EditCategoryModal
