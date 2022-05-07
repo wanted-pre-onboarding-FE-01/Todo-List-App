f351e1d9cc2b3ced9cc28ad3942f496c2c024c4b
GAEUN SHIN <newsilver1028@naver.com>
2022-05-07 23:37:18 +0900

    components/Todo 파일 추가

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types'
+import { BsCheckCircleFill, BsCircle } from 'react-icons/bs'
+import styles from './Todo.module.scss'
+import { cx } from '../../styles'
+
+function Todo({ todoList, category, handleTodoClick, handleChange, handleDeleteClick }) {
+  const { id, todo, categoryId, date, isDone } = todoList
+  const todoCategoryColor = category.filter((todoCategory) => todoCategory.id === categoryId)[0].color
+
+  // const [openEditModal]
+
+  return (
+    <li key={id} className={cx(styles.task, isDone && styles.checked)} onClick={handleTodoClick} aria-hidden='true'>
+      <div className={styles.checkboxWrapper}>
+        <input type='checkbox' checked={isDone} data-id={id} onChange={handleChange} readOnly />
+        {isDone ? (
+          <BsCheckCircleFill className={styles.checkIcon} color={todoCategoryColor} />
+        ) : (
+          <BsCircle className={styles.notCheckedIcon} color={todoCategoryColor} />
+        )}
+      </div>
+      <div className={styles.titleWrapper}>
+        <p className={styles.title}>{todo}</p>
+        {isDone ? (
+          <div className={styles.lineThrough} />
+        ) : (
+          <div className={styles.buttonWrapper}>
+            <button className={styles.editButton} type='button'>
+              Edit
+            </button>
+            <button type='button' className={styles.deleteButton} data-id={id} onClick={handleDeleteClick}>
+              Delete
+            </button>
+          </div>
+        )}
+      </div>
+    </li>
+  )
+}
+
+export default Todo
+
+Todo.propTypes = {
+  todoList: PropTypes.shape({
+    id: PropTypes.string,
+    todo: PropTypes.string,
+    categoryId: PropTypes.string,
+    date: PropTypes.string,
+    isDone: PropTypes.bool,
+  }),
+  category: PropTypes.arrayOf(PropTypes.arrayOf),
+  handleTodoClick: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleDeleteClick: PropTypes.func,
+}
---
+++ src/components/Todo/Todos.jsx
@@ -0,0 +1,175 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import Todo from './Todo'
+import styles from './Todos.module.scss'
+import { getAllData, updateAllData, getUserByUserId } from '../../utils/data/localStorage'
+import PropTypes from 'prop-types'
+
+import { cx } from '../../styles'
+import { SearchIcon } from '../../assets/svgs'
+import EmptyTodoList from './EmptyTodoList'
+
+const DATA = [
+  {
+    id: '1234sol',
+    userNickName: 'sol',
+    isLogined: true,
+    data: {
+      category: [
+        { id: '1School', categoryName: 'School', color: '#A8A8A8' },
+        { id: '2Business', categoryName: 'Business', color: '#7373eb' },
+        { id: '3zip', categoryName: 'zip', color: '#FF5252' },
+      ],
+      todoList: [
+        { id: '1아이스크림먹기', todo: '아이스크림먹기', categoryId: '1School', date: '2022/05/04', isDone: false },
+        { id: '3과제하기', todo: '과제하기', categoryId: '2Business', date: '2022/05/03', isDone: false },
+        { id: '12기상하기', todo: '기상하기', categoryId: '2Business', date: '2022/05/06', isDone: false },
+        { id: '1234강의듣기', todo: '강의듣기', categoryId: '3zip', date: '2022/05/05', isDone: false },
+        { id: '123러닝 다녀오기', todo: '러닝 다녀오기', categoryId: '3zip', date: '2022/05/05', isDone: false },
+        {
+          id: '1아234이스크림먹기',
+          todo: '아이스크림먹기',
+          categoryId: '1School',
+          date: '2022/05/04',
+          isDone: false,
+        },
+        { id: '3과232342제하기', todo: '과제하기', categoryId: '1School', date: '2022/05/03', isDone: false },
+        { id: '12기상2357하기', todo: '기상하기', categoryId: '1School', date: '2022/05/06', isDone: false },
+        { id: '1234강232343455의듣기', todo: '강의듣기', categoryId: '1School', date: '2022/05/05', isDone: false },
+        {
+          id: '123러닝324345678989867 다녀오기',
+          todo: '러닝 다녀오기',
+          categoryId: '1School',
+          date: '2022/05/05',
+          isDone: false,
+        },
+      ],
+    },
+  },
+  {
+    id: '1234sol33',
+    userNickName: 'sol33',
+    isLogined: false,
+    data: {
+      category: [
+        { id: '1School', categoryName: 'School', color: '#A8A8A8' },
+        { id: '2Business', categoryName: 'Business', color: '#7373eb' },
+        { id: '3zip', categoryName: 'zip', color: '#FF5252' },
+      ],
+      todoList: [
+        { id: '1아이스크림먹기', todo: '아이스크림먹기', categoryId: '1카테고리', date: '2022/05/04', isDone: false },
+        { id: '3과제하기', todo: '과제하기', categoryId: '3카테고리', date: '2022/05/03', isDone: false },
+        { id: '12기상하기', todo: '기상하기', categoryId: '1카테고리', date: '2022/05/06', isDone: false },
+        { id: '1234강의듣기', todo: '강의듣기', categoryId: '2카테고리', date: '2022/05/05', isDone: false },
+        { id: '123러닝 다녀오기', todo: '러닝 다녀오기', categoryId: '3카테고리', date: '2022/05/05', isDone: false },
+      ],
+    },
+  },
+]
+
+function Todos({ todoListState, setTodoListState, category }) {
+  const handleAddClick = (e) => {
+    // console.log('handleAddClick')
+  }
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+  }
+
+  const handleChange = (e) => {
+    const { dataset, checked } = e.currentTarget
+    const { id } = dataset
+
+    setTodoListState((prev) => {
+      const targetIndex = prev.findIndex((todo) => todo.id === id)
+      const firstDoneTodoIndex = prev.findIndex((todo) => todo.isDone)
+      const newList = [...prev]
+      newList[targetIndex].isDone = checked
+
+      if (!checked && firstDoneTodoIndex > -1) {
+        if (targetIndex === 0 && firstDoneTodoIndex === prev.length) {
+          return newList
+        }
+        const reorderedList = reorder(prev, targetIndex, firstDoneTodoIndex)
+        return reorderedList
+      }
+
+      if (checked) {
+        const reorderedList = reorder(prev, targetIndex, prev.length - 1)
+        return reorderedList
+      }
+
+      return newList
+    })
+  }
+
+  const handleDeleteClick = (e) => {
+    const { dataset } = e.currentTarget
+    const { id } = dataset
+    setTodoListState((prev) => {
+      const removedList = prev.filter((todo) => todo.id !== id)
+      return removedList
+    })
+  }
+
+  // search state and functions
+  const [searchOpen, setSearchOpen] = useState(false)
+
+  const handleToggleSearchBar = () => setSearchOpen((prev) => !prev)
+
+  const [searchValue, setSearchValue] = useState()
+
+  const handleChangeSearchValue = (e) => {
+    const {
+      currentTarget: { value },
+    } = e
+    setSearchValue(value)
+  }
+
+  return (
+    <ul className={styles.tasks}>
+      <div className={styles.header}>
+        <p className={styles.tasksTitle}>Today&apos;s</p>
+        <div className={cx(styles.searchContainer, searchOpen && styles.searchOpen)}>
+          {searchOpen && (
+            <input className={styles.searchInput} placeholder='Search to do...' onChange={handleChangeSearchValue} />
+          )}
+          <SearchIcon onClick={handleToggleSearchBar} />
+        </div>
+      </div>
+      <div className={styles.todosWrapper}>
+        {todoListState.length === 0 && <EmptyTodoList />}
+        {todoListState
+          .filter((todo) => {
+            if (!searchValue) {
+              return true
+            }
+            return todo.todo.includes(searchValue)
+          })
+          .map((todo) => (
+            <Todo
+              key={todo.id}
+              data-id={todo.id}
+              todoList={todo}
+              category={category}
+              handleChange={handleChange}
+              handleAddClick={handleAddClick}
+              handleDeleteClick={handleDeleteClick}
+            />
+          ))}
+      </div>
+    </ul>
+  )
+}
+
+export default Todos
+
+Todos.propTypes = {
+  todoListState: PropTypes.arrayOf(PropTypes.arrayOf),
+  setTodoListState: PropTypes.func,
+  category: PropTypes.arrayOf(PropTypes.arrayOf),
+}
