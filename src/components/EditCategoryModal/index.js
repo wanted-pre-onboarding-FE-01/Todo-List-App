032516abbab8764cf24e1f7b65819273b22adf5d
solchan98 <dev.sol0427@gmail.com>
2022-05-07 13:54:13 +0900

    feat: 컴포넌트 수정 및 삭제 데이터 연결 및 기능 연결 완료

--- src/components/EditCategoryModal/index.js
+++ src/components/EditCategoryModal/index.js
@@ -3,22 +3,25 @@ import PropTypes from 'prop-types'
 import { IoIosClose } from 'react-icons/io'
 import { useState } from "react"
 
-const fakeCategory = { id: '123Business', categoryName:'Business', color: '#566dda'}
-
-const moreColorList = [ 
+/** TODO
+ * MORE_COLOR_LIST와 BRIEF_COLOR_LIST는 다른 파트분들과 공통으로 사용되는 데이터이다.
+ * 따라서 데이터 관리를 논의해본 뒤, 리팩토링 예정이다.
+ */
+const MORE_COLOR_LIST = [ 
   { color: '#d4697b', isCheck: false }, { color: '#d27a56', isCheck: false }, { color: '#d1a345', isCheck: false }, { color: '#53a787', isCheck: false },
   { color: '#566dda', isCheck: false }, { color: '#8450da', isCheck: false }, { color: '#787d84', isCheck: false }, { color: '#f7d7de', isCheck: false },
   { color: '#f8dbcb', isCheck: false }, { color: '#f7eeb3', isCheck: false }, { color: '#d4f3d6', isCheck: false }, { color: '#d0e1fc', isCheck: false },
   { color: '#e0d1f7', isCheck: false }, { color: '#d9dbdb', isCheck: false }]
 
-const briefColorList = [ 
+const BRIEF_COLOR_LIST = [ 
   { color: '#d4697b', isCheck: false }, { color: '#d27a56', isCheck: false }, { color: '#d1a345', isCheck: false }, { color: '#53a787', isCheck: false },
   { color: '#566dda', isCheck: false }]
+  /** */
 
-function EditCategoryModal({ isShow, category = fakeCategory, close, edit, remove }) {
+function EditCategoryModal({ isShow, category, close, edit, remove }) {
 
   const [moreOn, setMoreOn] = useState(false)
-  const [colorList, setColorList] = useState(briefColorList)
+  const [colorList, setColorList] = useState(BRIEF_COLOR_LIST)
 
   const [selectColor, setSelectColor] = useState(category.color)
   const [categoryName, setCategoryName] = useState(category.categoryName)
@@ -28,7 +31,7 @@ function EditCategoryModal({ isShow, category = fakeCategory, close, edit, remov
 
   const moreButtonHandler = () => {
     setMoreOn(prev => !prev)
-    setColorList(prev => prev.length === moreColorList.length ? briefColorList : moreColorList)
+    setColorList(prev => prev.length === MORE_COLOR_LIST.length ? BRIEF_COLOR_LIST : MORE_COLOR_LIST)
   }
 
   const colorSelectHandler = (e) => {
@@ -48,8 +51,8 @@ function EditCategoryModal({ isShow, category = fakeCategory, close, edit, remov
           <IoIosClose color="#A8A8A8" className={styles.closeBtn} onClick={close}/>
         </div>
         <ul className={styles.colorCircleList} >
-          {colorList.map((color, key) => 
-            <li key={`color_index_${key + 3}`} className={styles.colorCircle}>
+          {colorList.map((color) => 
+            <li key={`color_index_${color.id}`} className={styles.colorCircle}>
               <button
                 type='button'
                 aria-label="Select color"
@@ -68,8 +71,8 @@ function EditCategoryModal({ isShow, category = fakeCategory, close, edit, remov
           <input type='text' value={categoryName} maxLength={20} placeholder={category.categoryName} onChange={categoryNameChangeHandler}/>
         </div>
         <div className={styles.btnsWrapper}>
-          <button type='button' className={styles.btn} onClick={() => edit(category, { categoryName, selectColor })}>Edit</button>
-          <button type='button' className={styles.btn} onClick={() => remove(category)}>Delete</button>
+          <button type='button' className={styles.btn} onClick={() => edit(category.id, categoryName, selectColor )}>Edit</button>
+          <button type='button' className={styles.btn} onClick={() => remove(category.id)}>Delete</button>
         </div>
       </div>
     </div>
