8d0811e4eb6c99b0c55f63317ec4b24c8b06e766
solchan98 <dev.sol0427@gmail.com>
2022-05-07 23:55:44 +0900

    refactor: setCategory를 넘겨받아 category 상태를 변경하도록 추가

--- src/hooks/EditCategoryModal.js
+++ src/hooks/EditCategoryModal.js
@@ -1,26 +1,36 @@
 import { useState } from 'react'
-import { editCategory, removeCategory } from '../utils/data/localStorage'
+import { editCategory, removeCategory, getUserByNickName } from '../utils/data/localStorage'
+
+const INIT_CATEGORY = {id: -1, categoryName: '', color: '#FFFFFF'}
 
 export const useEditCategoryModal = (nickName) => {
   const [isShow, setIsShow] = useState(false)
+  const [selectedCategory, setSelectedCategory] = useState(INIT_CATEGORY)
 
-  const open = () => {
-    setIsShow(isShow)
+  const open = (category) => {
+    setIsShow(true)
+    setSelectedCategory(category)
   }
 
   const close = () => {
-    setIsShow(!isShow)
+    setIsShow(false)
   }
 
-  const edit = (id, newCategoryName, newColor) => {
+  const edit = (id, newCategoryName, newColor, setCategory, setCategoryName) => {
     editCategory(nickName, {id, newCategoryName, newColor})
+    const storageData = getUserByNickName(nickName)
+    setCategory(storageData.data.category)
+    setCategoryName('')
     close()
   }
 
-  const remove = (categoryId) => {
+  const remove = (categoryId, setCategory, setCategoryName) => {
     removeCategory(nickName, categoryId)
+    const storageData = getUserByNickName(nickName)
+    setCategory(storageData.data.category)
+    setCategoryName('')
     close()
   }
 
-  return [isShow, open, close, edit, remove]
+  return [isShow, selectedCategory, open, close, edit, remove]
 }
