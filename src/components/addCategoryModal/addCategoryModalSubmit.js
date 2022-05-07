ba55fc200c357d5594faf09ca5f6af730be4f1ed
rlaebqebq <rlaebqebq@gmail.com>
2022-05-07 13:27:58 +0900

    feat: colorPicker 완성버전

--- src/components/addCategoryModal/addCategoryModalSubmit.js
+++ src/components/addCategoryModal/addCategoryModalSubmit.js
@@ -2,54 +2,60 @@ import styles from './AddCategoryModal.module.scss'
 import { useState } from 'react'
 import clsx from 'clsx'
 
+/* import { PopoverPicker } from './PopoverPicker' */
+
 function Modalsubmit() {
   const [color, setColor] = useState('#ffffff')
 
-  const colorarr = [
-    { name: 'red', value: '#ff3b30' },
-    { name: 'orange', value: '#ff9500' },
-    { name: 'yellow', value: '#ffcc00' },
-    { name: 'green', value: '#34c759' },
-    { name: 'blue', value: '#007aff' },
-    { name: 'purple', value: '#af52de' },
+  const colorArr = [
+    { colorName: 'red', colorValue: '#ff3b30' },
+    { colorName: 'orange', colorValue: '#ff9500' },
+    { colorName: 'yellow', colorValue: '#ffcc00' },
+    { colorName: 'green', colorValue: '#34c759' },
+    { colorName: 'blue', colorValue: '#007aff' },
+    { colorName: 'purple', colorValue: '#af52de' },
   ]
-  const tmp = '#ffffff'
+
   function handleForm(event) {
-    event.preventDefault() // stops form from "refreshing" automatically - it follows action, hence the refresh
-    console.log(`${event.target.name.value}`)
-    console.log(`${event.target.wow.value}`)
-    event.target.name.value = '' // flushes the name value
-    event.target.wow.value = '' // flushes the wow value
+    event.preventDefault() // 콘솔출력용
+    console.log(`${event.target.categoryName.value}`)
+    console.log(`${event.target.color.value}`)
+    event.target.categoryName.value = '' // flushes the name value
+    event.target.color.value = '' // flushes the wow value
   }
 
   return (
     <div className={styles.formStyle}>
       <form id='someForm' onSubmit={handleForm}>
-        <div className={styles.colorButtons}>
-          {colorarr.map((colorButton) => (
+        <div className={styles.colorPalette}>
+          {colorArr.map((colorPrepare) => (
             <button
-              aria-label='rainbowButton'
+              className={clsx(styles.colorPrepare)}
               type='button'
-              onClick={() => setColor(colorButton.value)}
-              key={`colorButton${colorButton.name}`}
-              className={clsx(styles.colorPalette)}
+              key={`colorPrepare${colorPrepare.colorName}`}
+              onClick={() => setColor(colorPrepare.colorValue)}
+              aria-label='colorPrepare'
             />
           ))}
 
-          <input
-            type='color'
-            value={color}
-            name='wow'
-            className={styles.colorPicker}
-            onChange={(e) => {
-              setColor(e.target.value)
-            }}
-          />
+          {/* <PopoverPicker color={color} onChange={setColor} /> */}
+
+          <div className={styles.hiddenBox}>
+            <input
+              className={styles.colorPicker}
+              type='color'
+              name='color'
+              value={color}
+              onChange={(e) => {
+                setColor(e.target.value)
+              }}
+            />
+          </div>
         </div>
         <input
           className={styles.categoryName}
-          name='name'
           type='text'
+          name='categoryName'
           maxLength='20'
           placeholder='카테고리 이름 입력'
           required
