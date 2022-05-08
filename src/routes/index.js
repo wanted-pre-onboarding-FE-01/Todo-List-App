2239830f82b9b5c68e7f1fd0f587b398b7e9b898
Kate Jung <69146527+katej927@users.noreply.github.com>
2022-05-08 14:02:24 +0900

    Revert "feat"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-import { Route, Routes } from 'react-router-dom'
-import Calendar from './Calendar'
-import Login from './Login'
-import styles from './Routes.module.scss'
-import TodoList from './TodoList'
-import UpdateTodo from './UpdateTodo'
+import { Route, Routes } from "react-router-dom";
+import Calendar from "./Calendar";
+import Login from "./Login";
+import styles from "./Routes.module.scss";
+import TodoList from "./TodoList";
 
 function App() {
   return (
@@ -13,11 +12,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<TodoList />} />
           <Route path="/calendar" element={<Calendar />} />
-          <Route path='/updateTodo' element={<UpdateTodo />} />
         </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
